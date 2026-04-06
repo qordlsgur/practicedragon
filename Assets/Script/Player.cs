d28f94432b273800536204720cea85b31ba59b98
fbtk <hyen6321@naver.com>
2026-04-06 21:02:39 +0900

    1일 차
    플레이어 이미지 추가 및 세팅

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,30 +2,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [System.Serializable] private struct AttackColliderOffset
+    {
+        public Vector2 AttackCollideroffset;
+        public Vector2 NewAttackCollideroffset;
+    }
+
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private bool MoveAnim = false;
-    
+
     private Rigidbody2D rb;
     private Animator anim;
+    private SpriteRenderer spriteRenderer;
 
-    private Vector2 MoveInput;
-    private Vector2 Transform;
+    private BoxCollider2D[] attackCollider;
+    [SerializeField] private AttackColliderOffset AttackOffset;
+
+
+    [SerializeField] private Vector2 MoveInput;
+    private Vector2 Movedelta;
 
     private void Awake()
     {
         moveSpeed = 10f;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        attackCollider = GetComponents<BoxCollider2D>();
+        AttackOffset.AttackCollideroffset = attackCollider[0].offset;
     }
 
     private void Start()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + Transform);
+        rb.MovePosition(rb.position + Movedelta);
     }
 
     private void Update()
@@ -33,15 +47,32 @@ public class Player : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        MoveInput = new Vector2 (x, y);
-        Transform = MoveInput.normalized * moveSpeed * Time.deltaTime;
+        MoveInput = new Vector2(x, y);
+        Movedelta = MoveInput.normalized * moveSpeed * Time.deltaTime;
 
         if (x != 0 || y != 0)
             MoveAnim = true;
         else
             MoveAnim = false;
 
+
+        if (MoveInput.x < 0)
+        {
+            // ¿ÞÂÊ
+            spriteRenderer.flipX = true;
+        }
+        else if (MoveInput.x > 0)
+        {
+            // ¿À¸¥ÂÊ
+            spriteRenderer.flipX = false;
+        }
+
+        AttackOffset.NewAttackCollideroffset.x = spriteRenderer.flipX ? -AttackOffset.AttackCollideroffset.x : AttackOffset.AttackCollideroffset.x;
+        attackCollider[0].offset = AttackOffset.NewAttackCollideroffset;
+
         anim.SetBool("Run", MoveAnim);
+
+
     }
 
     private void LateUpdate()
